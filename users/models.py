a2dc598e29c1ce638c4a0dcc0a2f839612a4ead9
John Han <work.johnhan@gmail.com>
2025-08-04 10:10:04 +0900

    FEAT: user => serializers, LoginView 구현

--- users/models.py
+++ users/models.py
@@ -10,11 +10,14 @@ class Provider(models.TextChoices):
 # Create your models here.
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    username = models.CharField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, unique=True, default="파운데이션 러너")
     provider = models.CharField(max_length=255, choices=Provider.choices, default=Provider.APPLE)
     rank = models.IntegerField(default=0)
     total_time = models.FloatField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    social_id = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    social_email = models.EmailField(max_length=255, null=True, blank=True)
 
     first_name = None
     last_name = None
--- users/serializers.py
+++ users/serializers.py
@@ -4,6 +4,6 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'provider', 'rank', 'total_time', 'created_at']
+        fields = ['id', 'username', 'provider', 'rank', 'total_time', 'created_at', 'social_id', 'social_email']
 
         read_only_fields = ['id', 'created_at']
