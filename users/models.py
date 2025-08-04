1f1179a89b800f9c31ab9b104aabd4f1b0650396
John Han <work.johnhan@gmail.com>
2025-08-04 09:23:57 +0900

    FEAT: User Model 명시

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+class Provider(models.TextChoices):
+    KAKAO = "kakao"
+    GOOGLE = "google"
+    APPLE = "apple"
 
 # Create your models here.
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=255, unique=True)
+    provider = models.CharField(max_length=255, choices=Provider.choices, default=Provider.APPLE)
+    rank = models.IntegerField(default=0)
+    total_time = models.FloatField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
