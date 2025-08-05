46a7b4a17a0b8766940d60ad66085d30e21cb4b8
John Han <work.johnhan@gmail.com>
2025-08-05 17:19:16 +0900

    FEAT: 유저 서치 기능 추가

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SocialLoginView
+from .views import SocialLoginView, UserSearchView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('auth/social-login/', SocialLoginView.as_view(), name='social-login'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path("search/", UserSearchView.as_view()),
 ]
