d207824e43008815adf946ecf5f4fe0ca615441b
John Han <work.johnhan@gmail.com>
2025-08-05 11:30:41 +0900

    FIX: view 오류 수정 및 마이그레이션

--- routines/views.py
+++ routines/views.py
@@ -2,6 +2,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 from .serializers import RoutineSerializer
 from .models import Routine
@@ -15,8 +16,23 @@ class RoutineView(APIView):
         serializer = RoutineSerializer(routines, many=True)
         return Response(serializer.data)
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    def post(self, request):
+        serializer = RoutineSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+class RoutineDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk, user):
+        return get_object_or_404(Routine, pk=pk, user=user)
+
+    def get(self, request, pk):
+        routine = self.get_object(pk, request.user)
+        serializer = RoutineSerializer(routine)
+        return Response(serializer.data)
 
     def patch(self, request, pk):
         routine = self.get_object(pk, request.user)
